cd5f39000675cd279fd41573516692ad68df7911
navi <navi@navi-laptop.(none)>
2013-03-15 19:15:31 +0800

    1. create a kernel module of cdata.c.
    	2. add three functions of fops as stub functions.
    
    	3. add register char device function to register in
    	   cdata_init_module().
    	4. add unregister function in cdata_cleanup_module().

---
+++ cdata.c
@@ -0,0 +1,75 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cdata.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/15/2013 06:43:42 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/delay.h>
+#include <linux/vmalloc.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/mm.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/miscdevice.h>
+#include <linux/input.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+static int cdata_open(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+ssize_t cdata_write(struct file *filp, const char *buf, size_t size, 
+                      loff_t *off)
+{
+	return 0;
+}
+
+int cdata_close(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static struct file_operations cdata_fops = {
+	open:		cdata_open,
+	release:	cdata_close,
+	write:		cdata_write,
+};
+
+int cdata_init_module(void)
+{
+	if(register_chrdev(121, "cdata", &cdata_fops) < 0){
+		printk(KERN_INFO "CDATA: can't register driver\n");
+		return -1;
+	}
+	return 0;
+}
+
+void cdata_cleanup_module(void)
+{
+	unregister_chrdev(121, "cdata");
+}
+
+module_init(cdata_init_module);
+module_exit(cdata_cleanup_module);
+
+MODULE_LICENSE("GPL");
