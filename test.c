c0cac9ddd348cb4ee32beab0874b1b84ce3ee5fa
navi <navi@navi-laptop.(none)>
2013-03-15 23:47:02 +0800

    App of test.c is a app for testing cdata kernel driver in open()

---
+++ test.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/15/2013 11:42:32 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+int main(void)
+{
+	int fd;
+
+	fd = open("/dev/cdata", O_RDWR);
+	close(fd);
+
+	return 0;
+}
+
